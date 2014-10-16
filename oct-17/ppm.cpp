0e3386577231e73037c0421d925c594f62530ae9
Ivan Zamanov <ivan.zamanov@asteasolutions.net>
2014-10-16 23:56:58 +0300

    Working PPM image

--- oct-17/ppm.cpp
+++ oct-17/ppm.cpp
@@ -32,9 +32,9 @@ public:
     // Read width, height and max value, assume is 255 or lower
     input >> width;
     input >> height;
-    std::cout << "width = " << width << "\nheight = " << height;
     input >> buffer;
     data = new Pixel[width * height];
+    input.get();
     input.read((char*) data, width * height * sizeof(Pixel));
     input.close();
   };
@@ -43,13 +43,13 @@ public:
   };
 
   Pixel& operator()(int x, int y) {
-    return data[x * width + y];
+    return data[y * width + x];
   };
 
   int getWidth() const { return width; };
   int getHeight() const { return height; };
 
-  void print(std::ostream& output) {
+  void print(std::ofstream& output) {
     output << "P6\n";
     output << width << '\n';
     output << height << '\n';
@@ -60,13 +60,14 @@ public:
 
 int main() {
   Image img(255, 255);
-  for(int i=0; i<img.getHeight(); i++) {
-    for(int j=0; j<img.getWidth(); j++) {
-      img(i,j)(i, j, i & j);
+  for(int x=0; x<img.getWidth(); x++) {
+    for(int y=0; y<img.getHeight(); y++) {
+      img(x,y)(x, y, x & y);
     }
   }
   std::ofstream output("test.ppm"), output2("test2.ppm");
   img.print(output);
+  output.close();
 
   Image img2("test.ppm");
   img2.print(output2);
